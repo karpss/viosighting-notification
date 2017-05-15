2bda887536cd3e95299ef213aae482be055a46b5
fam246 <ola.fam10@gmail.com>
2017-05-15 19:43:10 +0100

    Tweets now detected properly in real time

--- app/Console/Commands/VioSighting.php
+++ app/Console/Commands/VioSighting.php
@@ -4,21 +4,27 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 
+use GuzzleHttp\Subscriber\Oauth\Oauth1;
+use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
+
 class VioSighting extends Command
 {
+    private $api_endpoint = "https://stream.twitter.com/1.1/";
+
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'vio:sighting';
+    protected $signature = 'vio:sight';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Twitter Hashtag Notification';
 
     /**
      * Create a new command instance.
@@ -37,6 +43,61 @@ class VioSighting extends Command
      */
     public function handle()
     {
-        //
+        $search_keyword = "#viosighting";
+        $this->info("Twitter messages with the hashtag {$search_keyword}");
+
+        $message = $this->SearchTwitter($search_keyword);
+
+        while (!$message->eof()) {
+            $vio_tweet = json_decode($this->readLine($message), true);
+            echo $vio_tweet['text'] . PHP_EOL;
+        }
+    }
+
+    public function SearchTwitter($locate)
+    {
+        $my_defaults = [];
+
+        $my_stack = HandlerStack::create();
+        $oauth = new Oauth1([
+            'consumer_key'    => '',
+            'consumer_secret' => '',
+            'token'           => '',
+            'token_secret'    => '',
+        ]);
+        $my_stack->push($oauth);
+        $my_defaults = array_merge($my_defaults, [
+            'base_uri' => $this->api_endpoint,
+            'handler'  => $my_stack,
+            'auth'     => 'oauth',
+            'stream'   => true,
+        ]);
+        $this->client = new Client($my_defaults);
+
+        $result = $this->client->post('statuses/filter.json', [
+            'form_params' => [
+                'track' => $locate,
+            ],
+        ]);
+
+        return $result->getBody();
+    }
+
+    public function readLine($stream, $maxLength = null, $eol = PHP_EOL)
+    {
+        $buffer    = '';
+        $size      = 0;
+        $negEolLen = -strlen($eol);
+        while (!$stream->eof()) {
+            if (false === ($byte = $stream->read(1))) {
+                return $buffer;
+            }
+            $buffer .= $byte;
+
+            if (++$size == $maxLength || substr($buffer, $negEolLen) === $eol) {
+                break;
+            }
+        }
+        return $buffer;
     }
 }
